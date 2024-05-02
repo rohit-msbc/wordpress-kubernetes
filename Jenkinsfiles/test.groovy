799df2410aad648f80f7392a2d5220da848d6ada
rohit-msbc <144948139+rohit-msbc@users.noreply.github.com>
2024-05-02 21:31:34 +0530

    Update test.groovy

--- Jenkinsfiles/test.groovy
+++ Jenkinsfiles/test.groovy
@@ -6,7 +6,29 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/rohit-msbc/wordpress-kubernetes.git'
             }
         }
-        stage('Deploy to Minikube') {
+        stage('Deploy to Volume/Claim') {
+            steps {
+                script {
+                    sh '''kubectl applu -f 
+                    '''
+                }
+            }
+        }
+        stage('Deploy to secret') {
+            steps {
+                script {
+                    sh 'kubectl version'
+                }
+            }
+        }
+        stage('Deploy to mysql') {
+            steps {
+                script {
+                    sh 'kubectl version'
+                }
+            }
+        }
+        stage('Deploy to wordpress') {
             steps {
                 script {
                     sh 'kubectl version'
