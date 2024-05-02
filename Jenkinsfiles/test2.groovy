b2b9445d486752ae1e5043b02a68f4bf2b7932ee
rohit-msbc <144948139+rohit-msbc@users.noreply.github.com>
2024-05-02 21:15:26 +0530

    Update test2.groovy

--- Jenkinsfiles/test2.groovy
+++ Jenkinsfiles/test2.groovy
@@ -1,17 +1,15 @@
 pipeline {
     agent any
-    
     stages {
         stage('Clone Repository') {
             steps {
-                git 'https://github.com/rohit-msbc/wordpress-kubernetes.git'
+                git branch: 'main', url: ''
             }
         }
         stage('Deploy to Minikube') {
             steps {
                 script {
-                    def kubeConfig = readFile("${env.HOME}/.kube/config")
-                    sh "kubectl --kubeconfig=${kubeConfig} apply -f deployment.yml"
+                    sh 'kubectl apply -f deployment.yml'
                 }
             }
         }
