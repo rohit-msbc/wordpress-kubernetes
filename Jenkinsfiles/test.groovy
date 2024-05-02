745bc792e898dcc25dce0d171773b41987a32314
rohit-msbc <144948139+rohit-msbc@users.noreply.github.com>
2024-05-02 23:37:57 +0530

    Update test.groovy

--- Jenkinsfiles/test.groovy
+++ Jenkinsfiles/test.groovy
@@ -9,7 +9,29 @@ pipeline {
         stage('Deploy to pvc') {
             steps {
                 script {
-                    sh 'date'
+                    sh 'kubectl apply -f pvc.yml'
+                }
+            }
+        }
+        stage('Deploy to sql') {
+            steps {
+                script {
+                    sh 'kubectl apply -f sql-deploy.yml'
+                }
+            }
+        }
+        stage('Deploy to wordpress') {
+            steps {
+                script {
+                    sh 'kubectl apply -f wordpress-deploy.yml'
+                }
+            }
+        }
+        
+        stage('Deploy to expose') {
+            steps {
+                script {
+                    sh 'kubectl apply -f expose.yml'
                 }
             }
         }
